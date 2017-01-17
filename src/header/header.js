02be2a17ba5cede0bfed2faec81d96eb50253c39
James Leung <James.Leung@capitalone.com>
2017-01-16 22:52:27 -0500

    Changed Header

--- src/header/header.js
+++ src/header/header.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './header.css';
-import Owls from './owls.jpg'
 
 class Header extends Component {
     render() {
         return (
-            <img alt="Owls" src={Owls} className="header" />
+            <nav className="nav-bar">
+                <h1 id="name">James Leung</h1>
+                <ul className="desc">
+                    <li className="desc-item">Student</li>
+                    <li className="desc-item">Teacher</li>
+                    <li className="desc-item">Maker</li>
+                </ul>
+            </nav>
         )
     }
 };
