2d6fdc21ebc95a8c7ddd469a0b281a60ffda0d76
James Leung <James.Leung@capitalone.com>
2017-01-18 21:19:25 -0500

    fade only once by waypoint

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import About from './about/about';
 import Cornell from './cornell/cornell';
 import Waypoint from 'react-waypoint';
 
+let not_faded_yet = true;
+
 class App extends Component {
   _handleWaypointEnter() {
-    const element = document.getElementsByClassName("nav-content")[0];
-    let op = 0.1;  // initial opacity
-    let timer = setInterval(() => {
-      if (op >= 1) {
-        clearInterval(timer);
-      } else {
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.01;
-      }
-    }, 10);
+    if (not_faded_yet) {
+      const element = document.getElementsByClassName("nav-content")[0];
+      let op = 0.1;  // initial opacity
+      let timer = setInterval(() => {
+        if (op >= 1) {
+          clearInterval(timer);
+        } else {
+          element.style.opacity = op;
+          element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+          op += op * 0.02;
+        }
+      }, 10);
+      not_faded_yet = false;
+    }
   }
   render() {
     return (
